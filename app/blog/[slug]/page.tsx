d873e486f03722401e9baae6f0c4d181f5240ea1
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-01-09 17:04:19 +0400

    routing

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 interface BlogPageProps {
   params: {
@@ -9,8 +10,13 @@ function BlogPage({ params }: BlogPageProps) {
   return (
     <main>
       <h1>BlOG POST</h1>
+      <Link href="/blog">
+        <p>back</p>
+      </Link>
       <div>
-        <p>{params.slug}</p>
+        <p>
+          {params.slug === 'post-1' ? 'post number one' : 'post number two'}
+        </p>
       </div>
     </main>
   );
