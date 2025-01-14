5d32061f8c6ade3ea08481e25611e796c09a13a1
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-01-14 15:35:58 +0400

    header refactor

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -11,8 +11,12 @@ export default function Header() {
 
   return (
     <header className="flex items-center justify-between px-2 py-0 md:py-1">
-      <Link href="/">
+      <Link
+        href="/"
+        className="flex items-center justify-center gap-2 font-bold text-white md:text-xl"
+      >
         <Image src={logoImg.src} alt="Logo" width="40" height="40" />
+        <p className="hidden sm:block">Next Level Food</p>
       </Link>
       <nav>
         <ul className="m-0 flex list-none gap-2 p-0 font-bold no-underline active:bg-amber-600 active:bg-clip-text">
