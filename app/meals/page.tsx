b77b4903b8e510a732f0346bcc47e651e2adf310
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-01-15 18:39:40 +0400

    update meals page

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -3,18 +3,23 @@ import Link from 'next/link';
 
 function MealsPage() {
   return (
-    <div>
-      Meals
-      <Link href="/meals/slug1">
-        <p>slug1</p>
-      </Link>
-      <Link href="/meals/slug2">
-        <p>slug2</p>
-      </Link>
-      <Link href="/meals/share">
-        <p>Share</p>
-      </Link>
-    </div>
+    <>
+      <header className="mx-auto my-16 flex w-[90%] max-w-[75rem] flex-col gap-4">
+        <p className="font-primary text-2xl text-neutral">
+          Delicious meals, created{' '}
+          <span className="text-amber-500">by you.</span>
+        </p>
+        <p className="font-primary text-neutral">
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <Link
+          href="/meals/share"
+          className="inline-block w-fit rounded-md p-2 text-lg font-bold bg-gradient-link hover:bg-gradient-link"
+        >
+          Share your favorite recipe!
+        </Link>
+      </header>
+    </>
   );
 }
 
