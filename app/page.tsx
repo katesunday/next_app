89b80b289ce275149bb0305965aa889ccffc9295
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-01-15 15:55:34 +0400

    slide show

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,21 @@ export default function Home() {
           <ImageSlideShow />
         </div>
         <div>
-          <div className="text-neutral text-xl">
-            <h1 className="font-primary bg-gradient-to-r from-[#f9572a] to-[#ffc905] bg-clip-text text-2xl font-bold uppercase text-transparent">
+          <div className="text-xl text-neutral">
+            <h1 className="bg-gradient-to-r from-[#f9572a] to-[#ffc905] bg-clip-text font-primary text-2xl font-bold uppercase text-transparent">
               NextLevel Food for NextLevel Foodies
             </h1>
             <p>Taste & share food from all over the world.</p>
           </div>
           <div className="flex gap-4 text-xl">
             <Link
-              className="bg-gradient-link hover:bg-gradient-link mt-1 inline-block rounded-md px-4 py-2 font-bold text-white no-underline"
+              className="mt-1 inline-block rounded-md px-4 py-2 font-bold text-white no-underline bg-gradient-link hover:bg-gradient-link"
               href="/community"
             >
               Join the Community
             </Link>
             <Link
-              className="bg-gradient-link hover:bg-gradient-link mt-1 inline-block rounded-md px-4 py-2 font-bold text-white no-underline"
+              className="mt-1 inline-block rounded-md px-4 py-2 font-bold text-white no-underline bg-gradient-link hover:bg-gradient-link"
               href="/meals"
             >
               Explore Meals
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
       </header>
       <main>
-        <section className="max-w-200 text-neutral mx-auto my-4 flex w-[90%] flex-col text-center text-lg">
+        <section className="max-w-200 mx-auto my-4 flex w-[90%] flex-col text-center text-lg text-neutral">
           <h2 className="text-xl font-bold">How it works</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
@@ -45,7 +45,7 @@ export default function Home() {
           </p>
         </section>
 
-        <section className="max-w-200 text-neutral mx-auto my-4 flex w-[90%] flex-col text-center text-lg">
+        <section className="max-w-200 mx-auto my-4 flex w-[90%] flex-col text-center text-lg text-neutral">
           <h2 className="text-xl font-bold">Why NextLevel Food?</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
