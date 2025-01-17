5c854926704fbb5dee1937b52885947bd08f2f39
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-01-17 14:39:11 +0400

    not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-//also error
-//error.js => Fallback page for other errors (thrown by sibling pages or nested pages or layouts)
-//
-
-// loading.js => Fallback page which is shown whilst sibling or nested pages (or layouts) are fetching data
-//
-
-// route.js => Allows you to create an API route (i.e., a page which does NOT return JSX code but instead data, e.g., in the JSON format)
-
 function NotFound() {
-  return <div>I am lost. Page not found.</div>;
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
 }
 
 export default NotFound;
