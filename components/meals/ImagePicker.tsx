71ce5060cd4908e25f3e330d0e969520c6b7b578
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-01-28 18:01:02 +0400

    ImagePicker

--- components/meals/ImagePicker.tsx
+++ components/meals/ImagePicker.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 function ImagePicker({ label, name }: { label: string; name: string }) {
   return (
     <div>
-      <label htmlFor="image">{label}</label>
-      <div className="mb-4 flex items-start gap-8">
-        <input
-          type="file"
-          id="image"
-          accept="image/png, image/jpeg"
-          name={name}
-        />
-      </div>
+      {/*<label htmlFor="image">{label}</label>*/}
+      {/*<div className="mb-4 flex items-start gap-8">*/}
+      {/*  <input*/}
+      {/*    type="file"*/}
+      {/*    id="image"*/}
+      {/*    accept="image/png, image/jpeg"*/}
+      {/*    name={name}*/}
+      {/*  />*/}
+      {/*</div>*/}
     </div>
   );
 }
