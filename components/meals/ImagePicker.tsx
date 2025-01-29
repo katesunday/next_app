806436e2822c3c1f5e94d17770c040c735f6a60e
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-01-29 18:09:58 +0400

    ImagePicker update

--- components/meals/ImagePicker.tsx
+++ components/meals/ImagePicker.tsx
@@ -1,15 +1,32 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
 
 function ImagePicker({ label, name }: { label?: string; name: string }) {
   const ref = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState<string | ArrayBuffer | null>(null);
+
   const handleClick = () => {
     ref.current?.click();
+  };
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      return;
+    }
+    const file = e.target.files[0];
+
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      setImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   };
   return (
     <div>
       <label htmlFor="image">{label}</label>
       <div className="mb-4 flex items-start gap-8">
+        <div className="border-grey-50 relative flex h-40 w-40 items-center justify-center border-2 border-solid text-center">
+          {!image && <p>No image picked yet</p>}
+        </div>
         <input
           ref={ref}
           className="hidden"
@@ -17,6 +34,7 @@ function ImagePicker({ label, name }: { label?: string; name: string }) {
           id="image"
           accept="image/png, image/jpeg"
           name={name}
+          onChange={(e) => handleImageChange(e)}
         />
         <button
           className="bg-grey-30 hover:bg-grey-50 mt-4 cursor-pointer rounded border-0 px-4 py-2 text-inherit"
