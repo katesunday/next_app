6f0cf8b6b73d81eb8a925c268d63ab90df85a826
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-02-07 18:02:41 +0400

    refactor

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,14 +4,13 @@ import { MealType } from '@/initdb';
 import { getFormDataFile, getFormDataString } from '@/lib/helpers';
 
 export async function shareMeal(formData: FormData) {
-  const meal: Record<keyof MealType, string | File | null> = {
+  const meal: Record<keyof Omit<MealType, 'slug'>, string | File | null> = {
     title: getFormDataString(formData, 'title'),
     summary: getFormDataString(formData, 'summary'),
     instructions: getFormDataString(formData, 'instructions'),
     image: getFormDataFile(formData, 'image'),
     creator: getFormDataString(formData, 'name'),
     creator_email: getFormDataString(formData, 'email'),
-    slug: getFormDataString(formData, 'title')!.split('').join('_'),
   };
 
   console.log(meal);
--- lib/meals.ts
+++ lib/meals.ts
@@ -1,5 +1,6 @@
 import sql from 'better-sqlite3';
 import { MealType } from '@/initdb';
+import slugify from 'slugify';
 
 const db = sql('meals.db');
 
@@ -16,4 +17,11 @@ export async function getMeal(slug: string): Promise<MealType> {
     setTimeout(resolve, 2000);
   });
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug) as MealType;
+}
+
+export async function saveMeal(meal: MealType): Promise<void> {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+  const slug = slugify(meal.title);
 }
